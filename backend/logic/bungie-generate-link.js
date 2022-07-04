6be94330e291b953307d3de28dcac38539fcdf16
Dawwo-15 <davidemongera@hotmail.com>
2022-07-04 15:42:19 +0200

    Placeholder commit

--- backend/logic/bungie-generate-link.js
+++ backend/logic/bungie-generate-link.js
@@ -5,6 +5,7 @@ function generateAuthenticationLink() {
     const uri = "https://www.bungie.net/en/oauth/authorize?client_id="
         + process.env.OAUTH_CLIENT_ID + "&response_type=code&state=" + state;
     console.log(uri);
+    
 }
 
 generateAuthenticationLink();
