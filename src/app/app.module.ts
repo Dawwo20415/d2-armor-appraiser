dab7a31ca5cd14e564ac3c03d71dfe80deeadbd9
Dawwo-15 <davidemongera@hotmail.com>
2022-10-11 15:32:23 +0200

    Laid out template with instanced object for main page, now to reimplementing logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms'
+import { NgModule }                     from '@angular/core';
+import { BrowserModule }                from '@angular/platform-browser';
+import { HttpClientModule }             from '@angular/common/http';
+import { ReactiveFormsModule }          from '@angular/forms'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PageTemplateComponent } from './page-template/page-template.component';
-import { BAuthRedirectComponent } from './b-auth-redirect/b-auth-redirect.component';
+import { AppRoutingModule }             from './app-routing.module';
+import { AppComponent }                 from './app.component';
+import { PageTemplateComponent }        from './page-template/page-template.component';
+import { BAuthRedirectComponent }       from './b-auth-redirect/b-auth-redirect.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule} from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatGridListModule } from '@angular/material/grid-list'; 
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import { BrowserAnimationsModule }      from '@angular/platform-browser/animations';
+import { MatToolbarModule }             from '@angular/material/toolbar';
+import { MatIconModule }                from '@angular/material/icon';
+import { MatCardModule}                 from '@angular/material/card';
+import { MatButtonModule }              from '@angular/material/button';
+import { MatGridListModule }            from '@angular/material/grid-list'; 
+import { MatButtonToggleModule }        from '@angular/material/button-toggle';
+import { MatSliderModule }              from '@angular/material/slider';
+import { MatTableModule }               from '@angular/material/table';
 
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MainPageComponent } from './main-page/main-page.component';
-import { DisplayComponent } from './display/display.component';
-import { CharacterSelectComponent } from './homepage-cards/character-select/character-select.component';
+import { FlexLayoutModule }             from '@angular/flex-layout';
+import { MainPageComponent }            from './main-page/main-page.component';
+import { DisplayComponent }             from './display/display.component';
+import { CharacterSelectComponent }     from './homepage-cards/character-select/character-select.component';
 import { AlgorithmParametersComponent } from './homepage-cards/algorithm-parameters/algorithm-parameters.component';
-import { ScoreFilterComponent } from './homepage-cards/score-filter/score-filter.component';
-import { ArmorDataDisplayComponent } from './homepage-cards/armor-data-display/armor-data-display.component';
+import { ScoreFilterComponent }         from './homepage-cards/score-filter/score-filter.component';
+import { ArmorDataDisplayComponent }    from './homepage-cards/armor-data-display/armor-data-display.component';
 
 
 @NgModule({
@@ -50,6 +52,8 @@ import { ArmorDataDisplayComponent } from './homepage-cards/armor-data-display/a
     MatButtonModule,
     MatGridListModule,
     MatButtonToggleModule,
+    MatSliderModule,
+    MatTableModule,
 
     FlexLayoutModule 
   ],
--- src/app/homepage-cards/armor-data-display/armor-data-display.component.ts
+++ src/app/homepage-cards/armor-data-display/armor-data-display.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArmorItem } from '@dataTypes/storage-data.module'
 
 @Component({
   selector: 'app-armor-data-display',
@@ -11,5 +12,8 @@ export class ArmorDataDisplayComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  displayedColumns: string[] = ['ID','MOB','RES','REC','DIS','INT','STR','TOT','SCORE']
+  armorList: ArmorItem[] = [];
 
 }
