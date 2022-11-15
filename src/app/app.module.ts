da9deba71e2cac3aa35a767fc8425127d1dd67ac
Dawwo-15 <davidemongera@hotmail.com>
2022-11-15 18:46:58 +0100

    Added HashLocationStrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule }                     from '@angular/core';
 import { BrowserModule }                from '@angular/platform-browser';
 import { HttpClientModule }             from '@angular/common/http';
 import { ReactiveFormsModule }          from '@angular/forms'
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 // Components and Pages
 import { AppRoutingModule }             from './app-routing.module';
@@ -63,7 +64,7 @@ import { ClipboardModule }              from '@angular/cdk/clipboard';
     FlexLayoutModule,
     ClipboardModule 
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
