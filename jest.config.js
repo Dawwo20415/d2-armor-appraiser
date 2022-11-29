7c648bae048e6636c4b18d9be9ac3fde6e455bbc
Dawwo-15 <davidemongera@hotmail.com>
2022-11-29 13:25:25 +0100

    Created first Test suit

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    "@scripts/(.*)"   : "<rootDir>/src/scripts/$1",
+    "@assets/(.*)"    :"<rootDir>/src/assets/$1",
+    "@Ibrowser/(.*)"  : "<rootDir>/src/scripts/browser/$1",
+    "@Ibungie/(.*)"   : "<rootDir>/src/scripts/bungie-api-interaction/$1",
+    "@Bhashes/(.*)"   : "<rootDir>/src/scripts/bungie-api-interaction/hashes/$1",
+    "@algorithms/(.*)": "<rootDir>/src/scripts/scoring-algorithms/$1",
+    "@dataTypes/(.*)" : "<rootDir>/src/interfaces/$1"
+  }
 };
--- tests/stat-divergence.test.ts
+++ tests/stat-divergence.test.ts
@@ -1,8 +1,29 @@
-import {describe, expect, test} from '@jest/globals';
+import {describe, expect, test, beforeAll} from '@jest/globals';
+import Items from './armor-items-for-testing.json';
+import { statDivergence_v1 } from '@algorithms/stat-divergence';
+import { ArmorItem } from '@dataTypes/storage-data.module';
+
+let items = Items as ArmorItem[];
 
 describe('Testing Stat-Divergence-v1 algorithm', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(1+2).toBe(3);
+    beforeAll(() => {
+        items = statDivergence_v1(items, [1,1,1,1,1,1]);
+    });
+
+    test('Same Weights, Testing Distribution Homogenous vs Slightly Uneven', () => {
+        expect(items[0].score < items[1].score).toBe(true);
+    });
+
+    test('Same Weights, Testing Distribution Slightly Uneven vs Uneven', () => {
+        expect(items[1].score < items[2].score).toBe(true);
+    });
+
+    test('Same Weights, Testing Distribution Uneven vs Highly Uneven', () => {
+        expect(items[2].score < items[3].score).toBe(true);
+    });
+
+    test('Same Weights, Testing Distribution Highly Uneven vs Optimal', () => {
+        expect(items[3].score < items[4].score).toBe(true);
     });
     
 });
